aea1d97aea4b7e12b3f92d962d87252280433a59
Francisco Gonzalez-Martinez <paco_94@comunidad.unam.mx>
2019-06-04 19:06:30 -0700

    class camera created and open camera method

--- camjai.py
+++ camjai.py
@@ -7,3 +7,26 @@
 import os.path
 from ctypes import *
 #import numpy as np
+
+class CameraOpenError(Exception):
+    def __init__(self,mesg):
+        self.mesg=mesg
+    def __str__(self):
+        return self.mesg
+
+class Camera(object):
+    def __init__(self):
+        jai_file=os.path.dirname(os.path.realpath(__file__))+'\includesJAI\Jai_Factory.dll'
+        if os.path.isfile(jai_file):
+            self.handle=None
+            self.jaifactory=cdll.LoadLibrary(jai_file)
+        else:
+            raise CameraOpenError("Jai_Factory.dll file is missing")
+            
+    def open(self,name):
+        self.name=name
+        self.handle=c_int(0)
+        self.jaifactory.J_Factory_UpdateCameraList(self.handle)
+        
+
+
