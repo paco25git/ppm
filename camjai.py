ebe79563dac12e5b6adef2df4b050d3258d57e80
Francisco Gonzalez-Martinez <paco_94@comunidad.unam.mx>
2019-06-04 17:50:23 -0700

    JAI camera features added to the project: 1. creating the file camjai.py as a library for handle the camera (using ctypes for translate c++ variables to python)

---
+++ camjai.py
@@ -0,0 +1,9 @@
+#Title: camjai.py
+#Author:Francisco Gonzalez-Martinez
+#Date:06/04/2019
+
+#This is a library for handle the JAI camera: model AD-080GE
+
+import os.path
+from ctypes import *
+#import numpy as np
